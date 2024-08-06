a2271e41462402ff6c7339061ac7b2571d78ea2f
M Shafqat sarwar <khansarwar1@hotmail.com>
2024-08-07 02:48:12 +0500

    missing Question added number 7

--- Assignments/main.py
+++ Assignments/main.py
@@ -1,11 +1,11 @@
-# def className():
-#     print("Learning Python")
+def className():
+    print("Learning Python")
 
-#     def get_user_name(age: str):
-#         my_name: str = input("Enter your name: ")
-#         age = input("Enter your age!")
-#         print("Hello", my_name)
-#         return my_name
-#     new_user_name: str = get_user_name()
-#     print(f"Hello, {new_user_name}")
-# className()
+    def get_user_name(age: str):
+        my_name: str = input("Enter your name: ")
+        age = input("Enter your age!")
+        print("Hello", my_name)
+        return my_name
+    new_user_name: str = get_user_name()
+    print(f"Hello, {new_user_name}")
+className()
